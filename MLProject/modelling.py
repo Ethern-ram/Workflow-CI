a4744ae77c97257f49cce610815115ea329979aa
Erlangga Pradana Kurniawan <pradanaerlangga99@gmail.com>
2026-03-28 12:28:02 +0700

    Update dataset dan modelling untuk Titanic

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -12,9 +12,9 @@ def load_data(data_dir):
     if not os.path.exists(train_path):
         print(f"Data not found at {data_dir}. Menggunakan data dummy.")
         # Create dummy data for CI to not fail if data is not committed
-        X_train = pd.DataFrame(np.random.rand(100, 5))
+        X_train = pd.DataFrame(np.random.rand(100, 11))
         y_train = pd.Series(np.random.randint(0, 2, 100))
-        X_test = pd.DataFrame(np.random.rand(20, 5))
+        X_test = pd.DataFrame(np.random.rand(20, 11))
         y_test = pd.Series(np.random.randint(0, 2, 20))
         return X_train, X_test, y_train, y_test
 
@@ -28,7 +28,7 @@ def load_data(data_dir):
     return X_train, X_test, y_train, y_test
 
 def main():
-    X_train, X_test, y_train, y_test = load_data("breast_cancer_preprocessing")
+    X_train, X_test, y_train, y_test = load_data("titanic_preprocessing")
     
     with mlflow.start_run() as run:
         rf = RandomForestClassifier(n_estimators=50, random_state=42)
